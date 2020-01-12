c798f0bb52e89c70be98e0bcc0567c6e5141c7e0
Am-Coder <mishraaman2210@gmail.com>
2020-01-13 03:49:22 +0530

    URL structure configured

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import re_path
 from django.conf.urls import url
 from . import views
 
 app_name = 'gallery'
 
 urlpatterns = [
-    url(r'^/$', views, name='home'),
-    url(r'^()/$')
+
+    re_path(r'^/$', views, name='home'),
+    re_path(r'^(?P<album_id>[0-9]{4])/$', views, name='album-display'),
+    url(r'^logout/$', views, name='log-out'),
+    re_path(r'^create/$', views, name='create-album'),
+    re_path(r'^(?P<album_id>[0-9]{4])/upload$', views, name='upload-image'),
+
+    #Starring albums
+    re_path(r'^(?P<album_id>[0-9]{4}/star) $', views, name='star'),
+    re_path(r'^(?P<album_id>[0-9]{4}/unstar) $', views, name='unstar')
+
 ]
